9cdbf466f504ec6f7e8b352dad26fd18aa119266
Vail Dorchester <vdorchester@corp.exoanalytic.com>
2025-01-22 16:46:42 -0700

    added spring

--- player/first_person_controller.gd
+++ player/first_person_controller.gd
@@ -1,6 +1,7 @@
 extends CharacterBody3D
 
 @export var move_speed: float = 2.0
+@export var sprint_speed: float = 10.0
 @export var jump_impulse: float = 4.0
 @export var gravity: float = -9.81
 @export var mass: float = 100
@@ -23,25 +24,29 @@ func _process(_delta: float) -> void:
 		self.velocity.x = 0.0
 		self.velocity.z = 0.0
 	
+	var speed: float = move_speed
+	if Input.is_action_pressed("sprint"):
+		move_speed = sprint_speed
+	
 	if Input.is_action_pressed("move_forward"):
 		var forward: Vector3 = self.camera.basis.z * -1.0 
 		var forward_proj: Vector3 = Vector3(forward.x, 0.0, forward.z).normalized()
-		self.velocity += forward_proj * move_speed
+		self.velocity += forward_proj * speed
 	
 	if Input.is_action_pressed("move_backward"):
 		var backward: Vector3 = self.camera.basis.z
 		var backward_proj: Vector3 = Vector3(backward.x, 0.0, backward.z).normalized()
-		self.velocity += backward_proj * move_speed
+		self.velocity += backward_proj * speed
 		
 	if Input.is_action_pressed("move_left"):
 		var left: Vector3 = self.camera.basis.x * -1.0
 		var left_proj: Vector3 = Vector3(left.x, 0.0, left.z).normalized()
-		self.velocity += left_proj * move_speed
+		self.velocity += left_proj * speed
 		
 	if Input.is_action_pressed("move_right"):
 		var right: Vector3 = self.camera.basis.x
 		var right_proj: Vector3 = Vector3(right.x, 0.0, right.z).normalized()
-		self.velocity += right_proj * move_speed
+		self.velocity += right_proj * speed
 	
 	if Input.is_action_pressed("jump") and self.is_on_floor():
 		self.velocity += Vector3.UP * jump_impulse
