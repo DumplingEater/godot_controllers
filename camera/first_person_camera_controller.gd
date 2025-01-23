bfe6f5b3efd5f79fc8b3265f75f6190ac3936cfc
Vail Dorchester <vdorchester@corp.exoanalytic.com>
2025-01-23 13:48:44 -0700

    hid cursor

--- camera/first_person_camera_controller.gd
+++ camera/first_person_camera_controller.gd
@@ -7,7 +7,7 @@ var current_pitch: float = 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
