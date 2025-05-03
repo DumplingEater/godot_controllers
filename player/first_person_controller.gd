d93a7a6d1de7c35f82ff150239eff8c6ef77997c
Vail Dorchester <vail.paschal.dorchester@gmail.com>
2025-05-02 19:42:22 -0600

    im not sure

--- player/first_person_controller.gd
+++ player/first_person_controller.gd
@@ -12,7 +12,7 @@ var swimming: bool = false
 var locked: bool = false
 
 # Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+func _init() -> void:
 	add_to_group("player", true)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
